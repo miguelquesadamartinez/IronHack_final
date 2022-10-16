421b511c47316d51939d2ce8ec9579ca1a6e3db6
Miguel Quesada <miguel.quesada.martinez.1975@gmail.com>
2022-10-16 13:03:54 +0200

    Missing file

--- src/main/java/lab/miguel/code/MiguelApplication.java
+++ src/main/java/lab/miguel/code/MiguelApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiguelApplication extends SpringBootServletInitializer {
+public class MiguelApplication {
 
 	public static void main(String[] args) {
 
