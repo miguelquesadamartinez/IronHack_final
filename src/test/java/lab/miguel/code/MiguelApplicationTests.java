8a62cdae4e6d58652f5602972150dad4fa93c4eb
Miguel Quesada <miguel.quesada.martinez.1975@gmail.com>
2022-10-23 10:12:19 +0200

    Arreglamos los test de Mock, me quedo con error de ID´s, el que vengo arrastrando desde el principio

--- src/test/java/lab/miguel/code/MiguelApplicationTests.java
+++ src/test/java/lab/miguel/code/MiguelApplicationTests.java
@@ -104,7 +104,11 @@ class MiguelApplicationTests {
 
 	@Test
 	void mock_create_checking() throws Exception{
-		CreateCheckingDTO dto = new CreateCheckingDTO(500, 5l, 6l, LocalDate.now(), Status.ACTIVE, "12345",LocalDate.now(),  10, 100);
+		Address addr1 = addressRepository.save(new Address("Velia, 81"));
+		Address addr2 = addressRepository.save(new Address("Velia, 69"));
+		AccountHolders acc1 = accountHolderService.createAccountHolder(new CreateAccountHolderDTO("Miguel", "1975-04-18", addr1.getId(), addr2.getId()));
+		AccountHolders acc2 = accountHolderService.createAccountHolder(new CreateAccountHolderDTO("Pepe", "2000-02-01", addr1.getId(), addr2.getId()));
+		CreateCheckingDTO dto = new CreateCheckingDTO(500, acc1.getId(), acc2.getId(), LocalDate.now(), Status.ACTIVE, "12345",LocalDate.now(),  10, 100);
 
 		String body = objectMapper.writeValueAsString(dto);
 
@@ -126,11 +130,11 @@ class MiguelApplicationTests {
 		AccountHolders acc1 = accountHolderService.createAccountHolder(new CreateAccountHolderDTO("Miguel", "1975-04-18", addr1.getId(), addr2.getId()));
 		AccountHolders acc2 = accountHolderService.createAccountHolder(new CreateAccountHolderDTO("Pepe", "2000-02-01", addr1.getId(), addr2.getId()));
 
-		CreateCreditCardDTO dto = new CreateCreditCardDTO(500, acc1.getId(), acc2.getId(), LocalDate.now(), Status.ACTIVE, "12345",LocalDate.now(),  1000, 0.02);
+		CreateCreditCardDTO dto = new CreateCreditCardDTO(500, acc1.getId(), acc2.getId(), LocalDate.now(), Status.ACTIVE, "12345",LocalDate.now(),  1000, 0.2);
 
 		String body = objectMapper.writeValueAsString(dto);
 
-		MvcResult mvcResult = mockMvc.perform(post("/create-credit-card").content(body).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andReturn();
+		MvcResult mvcResult = mockMvc.perform(post("/create-credit-card").content(body).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
 
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode tree = mapper.readTree(mvcResult.getResponse().getContentAsString());
